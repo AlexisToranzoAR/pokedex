79883f2ddaa3c41592291b55ff1135a2ee871478
Alexis Toranzo <alextoranzo3@gmail.com>
2020-05-18 07:48:07 -0300

    Se realizo una prueba unitaria en utilidades
    Esta no se realiza debido a aparentemente un error en la conexion entre jest y babel

---
+++ src/utilidades/__tests__/utilidades.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="jest" />
+
+import firstCapitalLetter from '../utilidades.js';
+
+describe('utilidades', () => {
+    it('deberia pasar la primera letra a mayuscula', () => {
+        expect(firstCapitalLetter('prueba')).toEqual('Prueba');
+    });
+});
